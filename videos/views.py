850b1211f8adad91c33c90fb26cb944db797d815
Gabriel <gaboland1405@gmail.com>
2025-04-20 19:59:49 -0600

    Implementacion de inicio

--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Videos, VideosEtiquetas
 
-# Create your views here.
+def index(request):
+    return render(request, 'inicio.html')
