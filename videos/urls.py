5768eaa11f9e8912d94062041402b76b0d7586dc
Gabriel <gaboland1405@gmail.com>
2025-04-21 20:23:52 -0600

    Agregamos el form para videos. NO FUNCIONA AUN

--- videos/urls.py
+++ videos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns  = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('videos/upload/', views.form_video, name='form_video'),
 ]
--- videos/views.py
+++ videos/views.py
@@ -3,3 +3,6 @@ from .models import Videos, VideosEtiquetas
 
 def index(request):
     return render(request, 'inicio.html')
+
+def form_video(request):
+    return render(request, 'video.html')
