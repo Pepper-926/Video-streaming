c2f557b0039de9527f242a90a86fd0d991fd8ac6
Gabriel <gaboland1405@gmail.com>
2025-05-05 18:04:05 -0600

    Se mejoro la vista donde se visualiza el video, ya no recomienda el video que se visualiza actualmente.

---
+++ comentarios/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns  = [
+    path('registro/', views.registrar_usuario, name = 'registrar_usuario'),
+    path('login/', views.login, name = 'login'),
+    path('recuperar/', views.solicitar_recuperacion, name='solicitar_recuperacion'),
+    path('recuperar/<str:token>/', views.cambiar_contrasena, name='cambiar_contrasena'),
+]
